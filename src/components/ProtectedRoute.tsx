18b9d8f75e4046f45bad2be3a69533f5b981ec93
繁花落叶已成霜 <1035612457@qq.com>
2025-01-02 21:30:26 +0800

    feat: 本地存储accessToken 和 refreshToken

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,18 +1,30 @@
 import type { PropsWithChildren } from 'react'
-import { useEffect, useState } from 'react'
-
+import { message } from 'antd'
+import { useEffect, useRef } from 'react'
 import { useNavigate } from 'react-router'
+import { useUserInfo } from '../store/useUserInfo.tsx'
 
 export function ProtectedRoute(props: PropsWithChildren) {
   const { children } = props
-  const [isAuthenticated, setIsAuthenticated] = useState(true)
   const navigate = useNavigate()
+  const isAuthenticated = useRef(false) // 使用 useRef 存储 isAuthenticated
+  const accessToken = useUserInfo(state => state.accessToken)
+  const loadToken = useUserInfo(state => state.loadToken)
 
   useEffect(() => {
-    if (!isAuthenticated) {
-      navigate('/login')
+    if (accessToken) {
+      isAuthenticated.current = true // 更新 ref 的值
     }
-  }, [isAuthenticated])
+  }, [accessToken]) // 当 accessToken 变化时触发
+
+  useEffect(() => {
+    loadToken().then(() => {
+      if (!isAuthenticated.current) {
+        message.warning('请先登录')
+        navigate('/login')
+      }
+    })
+  }, [loadToken, accessToken, navigate])
 
   return children
 }
--- src/store/useUserInfo.tsx
+++ src/store/useUserInfo.tsx
@@ -1,3 +1,4 @@
+import * as localforage from 'localforage'
 import { create } from 'zustand'
 import { devtools } from 'zustand/middleware'
 
@@ -7,13 +8,39 @@ interface UserInfoStore {
 }
 
 interface UserInfoAction {
-  updateToken: (accessToken: string, refreshToken: string) => void
+  updateToken: (accessToken: string, refreshToken: string) => Promise<void>
+  loadToken: () => Promise<void>
 }
 
 export const useUserInfo = create<UserInfoStore & UserInfoAction>()(
   devtools(set => ({
-    token: '',
-    updateToken: (accessToken: string, refreshToken: string) => set({ accessToken, refreshToken }),
+    accessToken: '',
+    refreshToken: '',
+    updateToken: async (accessToken: string, refreshToken: string) => {
+      // 更新本地 Token
+      try {
+        await localforage.setItem('accessToken', accessToken)
+        await localforage.setItem('refreshToken', refreshToken)
+      }
+      catch (err) {
+        console.error(err)
+      }
+      return set({ accessToken, refreshToken })
+    },
+    loadToken: async () => {
+      try {
+        const accessToken: string | null = await localforage.getItem('accessToken')
+        const refreshToken: string | null = await localforage.getItem('refreshToken')
+        if (accessToken && refreshToken) {
+          return set({ accessToken, refreshToken })
+        }
+
+        return set({ accessToken: '', refreshToken: '' })
+      }
+      catch (err) {
+        console.error(err)
+      }
+    },
   }), {
     name: 'userInfo',
     enabled: true,
