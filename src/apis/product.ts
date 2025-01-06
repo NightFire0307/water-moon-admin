607e04dfdf6f1116549666246283bcb214d322b9
繁花落叶已成霜 <1035612457@qq.com>
2025-01-06 23:04:53 +0800

    feat: 完善产品类型管理

--- src/apis/product.ts
+++ src/apis/product.ts
@@ -1,24 +1,39 @@
 import type { Response } from '../types/common.ts'
+import type { ProductTypeResponse } from '../types/product.ts'
 import request from '../utils/request.ts'
 
-export function getProductList() {
+export function getProductList(): ProductTypeResponse {
   return request({
     url: '/api/admin/product',
-    method: 'get',
+    method: 'GET',
+  })
+}
+
+export function queryProductByName(name: string): ProductTypeResponse {
+  return request({
+    url: `/api/admin/product/type?name=${name}`,
+    method: 'GET',
   })
 }
 
 export function getProductTypes() {
   return request({
     url: '/api/admin/product/type',
-    method: 'get',
+    method: 'GET',
   })
 }
 
 export function createProductType(data: { name: string }): Promise<Response<string>> {
   return request({
     url: '/api/admin/product/type',
-    method: 'post',
+    method: 'POST',
     data,
+  })
+}
+
+export function deleteProductType(id: number): Promise<Response<string>> {
+  return request({
+    url: `/api/admin/product/type/${id}`,
+    method: 'DELETE',
   })
 }
