d9bfc9802067935f4a94f03b343ece97e22b7735
繁花落叶已成霜 <1035612457@qq.com>
2025-01-09 11:45:20 +0800

    fix: 修复Select组件Options类型错误

--- src/views/Order/OrderQueryForm.tsx
+++ src/views/Order/OrderQueryForm.tsx
@@ -11,10 +11,10 @@ export function OrderQueryForm(props: IOrderQueryFormProps) {
   const { onQuery, onReset } = props
 
   const options = [
-    { label: <Badge status="default" text="选片未开始" />, value: OrderStatus.NOT_STARTED },
-    { label: <Badge status="processing" text="选片进行中" />, value: OrderStatus.IN_PROGRESS },
-    { label: <Badge status="success" text="选片已完成" />, value: OrderStatus.SUBMITTED },
-    { label: <Badge status="error" text="链接已过期" />, value: OrderStatus.EXPIRED },
+    { label: <Badge status="default" text="选片未开始" />, value: OrderStatus.NOT_STARTED.toString() },
+    { label: <Badge status="processing" text="选片进行中" />, value: OrderStatus.IN_PROGRESS.toString() },
+    { label: <Badge status="success" text="选片已完成" />, value: OrderStatus.SUBMITTED.toString() },
+    { label: <Badge status="error" text="链接已过期" />, value: OrderStatus.EXPIRED.toString() },
   ]
 
   return (
@@ -28,7 +28,6 @@ export function OrderQueryForm(props: IOrderQueryFormProps) {
           label: '订单状态',
           name: 'status',
           type: 'select',
-          placeholder: '请选择',
           options,
         },
       ]}
