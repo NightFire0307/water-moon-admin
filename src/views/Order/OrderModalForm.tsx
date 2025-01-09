bffd7a1a557e9b858ceb61ff167ffc72ebe12c88
繁花落叶已成霜 <1035612457@qq.com>
2025-01-09 23:13:51 +0800

    完善Rules

--- src/views/Order/OrderModalForm.tsx
+++ src/views/Order/OrderModalForm.tsx
@@ -1,9 +1,9 @@
-import type { Field } from '@/components/CustomForm.tsx'
+import type { CustomFormRef, Field } from '@/components/CustomForm.tsx'
 import { CustomForm } from '@/components/CustomForm.tsx'
 import { ShareLink } from '@/views/Order/ShareLink.tsx'
 import { UploadPhoto } from '@/views/Order/UploadPhoto.tsx'
-import { Button, Modal, Space, Steps } from 'antd'
-import { useState } from 'react'
+import { Button, message, Modal, Result, Space, Steps } from 'antd'
+import { useRef, useState } from 'react'
 
 interface OrderModalFormProps {
   open: boolean
@@ -13,28 +13,42 @@ interface OrderModalFormProps {
 export function OrderModalForm(props: OrderModalFormProps) {
   const { open, onCancel } = props
   const [currentStep, setCurrentStep] = useState(0)
+  const [createLoading, setCreateLoading] = useState(false)
+  const formRef = useRef<CustomFormRef>(null)
 
   const fields: Field[] = [
     {
       label: '订单号',
       name: 'order_number',
       type: 'input',
+      rules: [{
+        required: true,
+        message: '请输入订单号',
+      }],
     },
     {
       label: '客户姓名',
       name: 'customer_name',
       type: 'input',
+      rules: [{
+        required: true,
+        message: '请输入客户姓名',
+      }],
     },
     {
       label: '客户手机',
       name: 'customer_phone',
       type: 'input',
+      rules: [{
+        required: true,
+        message: '请输入客户手机',
+      }],
     },
     {
       label: '产品选择',
       name: 'product_id',
       type: 'select',
-      mode: 'tags',
+      mode: 'multiple',
       options: [
         {
           label: '产品1',
@@ -43,6 +57,12 @@ export function OrderModalForm(props: OrderModalFormProps) {
         {
           label: '产品2',
           value: 2,
+        },
+      ],
+      rules: [
+        {
+          required: true,
+          message: '请选择产品',
         },
       ],
     },
@@ -70,10 +90,23 @@ export function OrderModalForm(props: OrderModalFormProps) {
     },
   ]
 
-  function handleNext() {
+  async function handleNext() {
     // TODO: 创建订单
+    try {
+      await formRef.current?.validateFields()
+      setCreateLoading(true)
+      const values = formRef.current?.getFormValues()
+      console.log(values)
 
-    setCurrentStep(currentStep + 1)
+      setTimeout(() => {
+        setCreateLoading(false)
+        setCurrentStep(currentStep + 1)
+      }, 2000)
+    }
+    catch (err) {
+      console.log(err)
+      message.error('表单校验失败，请检查')
+    }
   }
 
   function handlePrev() {
@@ -118,6 +151,7 @@ export function OrderModalForm(props: OrderModalFormProps) {
         {
           currentStep === 0 && (
             <CustomForm
+              ref={formRef}
               fieldCols={2}
               fields={fields}
               footer={null}
@@ -132,6 +166,29 @@ export function OrderModalForm(props: OrderModalFormProps) {
         {
           currentStep === 2 && <ShareLink />
         }
+
+        {
+          currentStep === 3 && (
+            <Result
+              status="success"
+              title="创建成功"
+              subTitle={(
+                <div>
+                  <p>您可以复制下面的链接，发送给客户，让客户查看照片</p>
+                  <div>
+                    <span>链接：https://example.com</span>
+                    {' '}
+                    <span>密码：123456</span>
+                    <p>链接有效期为7天，访问次数不限</p>
+                  </div>
+                </div>
+              )}
+              extra={[
+                <Button type="primary">复制链接</Button>,
+              ]}
+            />
+          )
+        }
       </div>
 
       <Space>
@@ -139,7 +196,7 @@ export function OrderModalForm(props: OrderModalFormProps) {
           currentStep > 0 && <Button onClick={handlePrev}>上一步</Button>
         }
         {
-          currentStep < 3 && <Button type="primary" onClick={handleNext}>下一步</Button>
+          currentStep < 3 && <Button type="primary" onClick={handleNext} loading={createLoading}>下一步</Button>
         }
         {
           currentStep === 3 && <Button type="primary">完成</Button>
