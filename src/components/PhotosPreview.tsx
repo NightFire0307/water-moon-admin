92c9af0bcc99202344c2e48226aead696c06a52f
繁花落叶已成霜 <1035612457@qq.com>
2025-02-05 15:19:58 +0800

    完善照片管理

--- src/components/PhotosPreview.tsx
+++ src/components/PhotosPreview.tsx
@@ -1,59 +1,152 @@
 import type { IPhoto } from '@/types/photo.ts'
-import { DeleteOutlined, StarFilled, StarOutlined } from '@ant-design/icons'
-import { Button, Checkbox, Image } from 'antd'
+import { BorderOutlined, CheckSquareOutlined, DeleteOutlined, StarFilled, StarOutlined } from '@ant-design/icons'
+import { Button, Checkbox, Divider, Flex, Image, Popconfirm, Space } from 'antd'
+import { useState } from 'react'
 import styles from './PhotoPreview.module.less'
-
-interface PhotosPreviewProps {
-  photoList: []
-}
 
 interface CustomMaskProps {
   isRecommend: boolean
+  isSelect: boolean
+  photoId: number
+  onSelect: (photoId: number) => void
+  onRemove: (photoId: number) => void
+  onRecommend: (photoId: number) => void
+  onUnRecommend: (photoId: number) => void
 }
 
 function CustomMask(props: CustomMaskProps) {
-  const { isRecommend } = props
+  const { isRecommend, photoId, isSelect, onRecommend, onUnRecommend, onRemove, onSelect } = props
 
   return (
-    <div className={styles['custom-mask']} onClick={e => e.stopPropagation()}>
-      <Checkbox />
-      <div className={styles['star-group']}>
+    <div className={styles['custom-mask']}>
+      <Checkbox
+        checked={isSelect}
+        onChange={() => onSelect(photoId)}
+        onClick={e => e.stopPropagation()}
+      />
+      <div className={styles['star-group']} onClick={e => e.stopPropagation()}>
         {
           isRecommend
-            ? <Button type="text" icon={<StarOutlined />} shape="circle" style={{ color: 'gold' }} />
-            : <Button type="text" icon={<StarFilled />} shape="circle" style={{ color: 'gold' }} />
+            ? <Button type="text" icon={<StarOutlined />} shape="circle" style={{ color: 'gold' }} onClick={() => onUnRecommend(photoId)} />
+            : <Button type="text" icon={<StarFilled />} shape="circle" style={{ color: 'gold' }} onClick={() => onRecommend(photoId)} />
         }
-        <Button type="text" icon={<DeleteOutlined />} shape="circle" danger />
+        <Popconfirm title="确定删除这张照片吗？" onConfirm={() => onRemove(photoId)} okText="确定" cancelText="取消">
+          <Button type="text" icon={<DeleteOutlined />} shape="circle" danger />
+        </Popconfirm>
       </div>
     </div>
   )
 }
 
-export function PhotosPreview(props: PhotosPreviewProps) {
-  const photosList: IPhoto[] = [
-    { id: 1, oss_url: '/src/assets/placeholder.svg', is_recommend: true, is_selected: false, created_at: '', updated_at: '' },
-  ]
+export function PhotosPreview() {
+  const [selectedPhotos, setSelectedPhotos] = useState<number[]>([])
+
+  const [photosList, setPhotoList] = useState<IPhoto[]>(Array.from({ length: 50 }).map((_, index) => ({
+    id: index + 1,
+    oss_url: '/src/assets/placeholder.svg',
+    is_recommend: false,
+    is_selected: false,
+    created_at: '',
+    updated_at: '',
+  })))
+
+  function handleSelect(photoId: number) {
+    setSelectedPhotos((prev) => {
+      const index = prev.indexOf(photoId)
+      if (index > -1) {
+        return prev.filter(id => id !== photoId)
+      }
+      return [...prev, photoId]
+    })
+  }
+
+  function handleSelectAll() {
+    setSelectedPhotos((prev) => {
+      if (prev.length === photosList.length) {
+        return []
+      }
+      return photosList.map(photo => photo.id)
+    })
+  }
+
+  function handleRecommend(photoId: number) {
+    setPhotoList((prev) => {
+      const photo = photosList.find(photo => photo.id === photoId)
+      if (photo) {
+        photo.is_recommend = true
+      }
+      return [...prev]
+    })
+  }
+
+  function handleUnRecommend(photoId: number) {
+    setPhotoList((prev) => {
+      const photo = photosList.find(photo => photo.id === photoId)
+      if (photo) {
+        photo.is_recommend = false
+      }
+      return [...prev]
+    })
+  }
+
+  function handleRemove(photoId: number) {
+    setPhotoList((prev) => {
+      return prev.filter(photo => photo.id !== photoId)
+    })
+  }
 
   return (
     <>
-      {
-        photosList.map(photo => (
-          <div style={{ display: 'inline-block', position: 'relative' }}>
-            <Image
-              src={photo.oss_url}
-              alt="placeholder"
-              preview={{
-                mask: <CustomMask isRecommend={photo.is_recommend} />,
-              }}
-              width={300}
-              height={300}
-            />
+      <Flex justify="space-between">
+        <Space>
+          <Button
+            icon={selectedPhotos.length === photosList.length ? <CheckSquareOutlined /> : <BorderOutlined />}
+            onClick={handleSelectAll}
+          >
             {
-              photo.is_recommend && <StarFilled style={{ color: 'gold', position: 'absolute', top: '8px', right: '8px' }} />
+              selectedPhotos.length === photosList.length ? '取消全选' : '全选'
             }
-          </div>
-        ))
-      }
+          </Button>
+          <Button color="gold" icon={<StarOutlined />}>标记精选</Button>
+        </Space>
+        <Button icon={<DeleteOutlined />} danger>删除</Button>
+      </Flex>
+      <Divider />
+      <div className={styles['photos-preview']}>
+        {
+          photosList.map(photo => (
+            <div
+              style={{
+                display: 'inline-block',
+                position: 'relative',
+                border: selectedPhotos.includes(photo.id) ? '2px solid #597ef7' : 'none',
+              }}
+              key={photo.id}
+            >
+              <Image
+                src={photo.oss_url}
+                alt="placeholder"
+                preview={{
+                  mask: (
+                    <CustomMask
+                      photoId={photo.id}
+                      isSelect={selectedPhotos.includes(photo.id)}
+                      isRecommend={photo.is_recommend}
+                      onSelect={handleSelect}
+                      onRecommend={handleRecommend}
+                      onUnRecommend={handleUnRecommend}
+                      onRemove={handleRemove}
+                    />
+                  ),
+                }}
+              />
+              {
+                photo.is_recommend && <StarFilled style={{ color: 'gold', position: 'absolute', top: '8px', right: '8px' }} />
+              }
+            </div>
+          ))
+        }
+      </div>
     </>
   )
 }
--- src/views/Order/PhotoMgr.tsx
+++ src/views/Order/PhotoMgr.tsx
@@ -1,8 +1,7 @@
 import { getPhotosByOrderId } from '@/apis/photo.ts'
 import { PhotosPreview } from '@/components/PhotosPreview.tsx'
-import { BorderOutlined, CheckSquareOutlined, DeleteOutlined, StarOutlined } from '@ant-design/icons'
-import { Button, Divider, Drawer, Flex, Space } from 'antd'
-import { useEffect, useState } from 'react'
+import { Drawer } from 'antd'
+import { useEffect } from 'react'
 
 interface PhotoMgrProps {
   open: boolean
@@ -12,7 +11,6 @@ interface PhotoMgrProps {
 
 export function PhotoMgr(props: PhotoMgrProps) {
   const { open, orderId, onClose } = props
-  const [isAllSelect, setIsAllSelect] = useState(false)
 
   async function getPhotos() {
     const res = await getPhotosByOrderId({ orderId })
@@ -26,22 +24,16 @@ export function PhotoMgr(props: PhotoMgrProps) {
   }, [open])
 
   return (
-    <Drawer open={open} title="照片管理" width="90%" getContainer={false} onClose={onClose}>
-      <Flex justify="space-between">
-        <Space>
-          <Button
-            icon={isAllSelect ? <CheckSquareOutlined /> : <BorderOutlined />}
-            onClick={() => setIsAllSelect(prev => !prev)}
-          >
-            {
-              isAllSelect ? '取消全选' : '全选'
-            }
-          </Button>
-          <Button color="gold" icon={<StarOutlined />}>标记精选</Button>
-        </Space>
-        <Button icon={<DeleteOutlined />} danger>删除</Button>
-      </Flex>
-      <Divider />
+    <Drawer
+      open={open}
+      title="照片管理"
+      width="90%"
+      getContainer={false}
+      onClose={onClose}
+      styles={{
+        body: { overflow: 'hidden' },
+      }}
+    >
       <PhotosPreview photoList={[]} />
     </Drawer>
   )
