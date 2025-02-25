39aa44bee442ceeca295fee709d40db3b0e939ba
繁花落叶已成霜 <1035612457@qq.com>
2025-02-25 17:03:40 +0800

    调整一下查看选片结果页面

--- src/views/Order/SelectionResult.tsx
+++ src/views/Order/SelectionResult.tsx
@@ -1,6 +1,13 @@
 import type { IPhoto } from '@/types/photo.ts'
 import type { MenuProps, TabsProps } from 'antd'
-import { DownloadOutlined, FileExcelOutlined, FilePdfOutlined, PictureOutlined } from '@ant-design/icons'
+import {
+  DownloadOutlined,
+  FileExcelOutlined,
+  FilePdfOutlined,
+  InfoCircleOutlined,
+  PictureOutlined,
+  StarOutlined,
+} from '@ant-design/icons'
 import { Button, Dropdown, Image, Space, Tabs } from 'antd'
 import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
@@ -15,11 +22,23 @@ function ImageGrid(props: ImageGridProps) {
   return (
     <div className={styles['image-grid']}>
       {
-        photos.map(_ => (
-          <Image
-            alt="placeholder"
-            src="/src/assets/placeholder.svg"
-          />
+        photos.map(photo => (
+          <div style={{ position: 'relative' }}>
+            <Image
+              alt="placeholder"
+              src={photo.thumbnail_url}
+              preview={{
+                mask: null,
+              }}
+            />
+            <div style={{ textAlign: 'center', marginTop: '6px', fontWeight: 'bold' }}>照片名称</div>
+            <div style={{ position: 'absolute', top: '6px', right: '6px', fontSize: '18px' }}>
+              <Space direction="vertical">
+                <StarOutlined />
+                <InfoCircleOutlined style={{ cursor: 'pointer' }} />
+              </Space>
+            </div>
+          </div>
         ))
       }
     </div>
@@ -29,6 +48,7 @@ function ImageGrid(props: ImageGridProps) {
 export function SelectionResult() {
   const { orderId } = useParams<{ orderId: string }>()
   const [tabsItem, setTabsItem] = useState<TabsProps['items']>([])
+  const [photos, setPhotos] = useState<IPhoto[]>([])
 
   const outPutItems: MenuProps['items'] = [
     {
@@ -52,14 +72,28 @@ export function SelectionResult() {
   useEffect(() => {
     console.log(orderId)
 
+    setPhotos((prev) => {
+      const newPhotos: IPhoto[] = []
+      for (let i = 0; i < 10; i++) {
+        newPhotos.push({
+          id: i,
+          file_name: `照片名称${i}`,
+          thumbnail_url: '/src/assets/placeholder.svg',
+          original_url: '/src/assets/placeholder.svg',
+          is_recommend: false,
+        })
+      }
+      return newPhotos
+    })
+
     setTabsItem([
       {
         label: '全部 (10)',
         key: 'all',
-        children: <ImageGrid photos={Array.from({ length: 30 })} />,
+        children: <ImageGrid photos={photos} />,
       },
       {
-        label: '未选 (0)',
+        label: '未选 (10)',
         key: 'unselected',
         children: 'Content of Tab Pane 4',
       },
@@ -82,7 +116,7 @@ export function SelectionResult() {
               menu={{ items: outPutItems }}
               type="primary"
             >
-              导出选片结果为
+              导出结果为
             </Dropdown.Button>
             <Button icon={<DownloadOutlined />}>打包下载</Button>
           </Space>
