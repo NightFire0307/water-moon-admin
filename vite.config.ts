b35972d20f8ed6407bf2783a95334f1192e82372
繁花落叶已成霜 <1035612457@qq.com>
2025-05-29 17:20:46 +0800

    feat: 添加 Dockerfile 和 Nginx 配置，更新构建和运行脚本

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,18 @@
 import react from '@vitejs/plugin-react'
+import { visualizer } from 'rollup-plugin-visualizer'
 import { defineConfig } from 'vite'
 import svgr from 'vite-plugin-svgr'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr({
-    svgrOptions: {
-      icon: true,
-    },
-  })],
+  plugins: [
+    react(),
+    svgr({
+      svgrOptions: {
+        icon: true,
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '@': '/src',
@@ -20,6 +24,34 @@ export default defineConfig({
       '/api': {
         target: 'http://localhost:3000',
         rewrite: path => path.replace(/^\/api/, ''),
+      },
+    },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          // 三大核心库拆包
+          if (id.includes('node_modules')) {
+            if (id.includes('dayjs'))
+              return 'dayjs'
+            if (id.includes('echarts'))
+              return 'echarts'
+            if (id.includes('zustand'))
+              return 'zustand'
+            if (id.includes('axios'))
+              return 'axios'
+            if (id.includes('@react-pdf'))
+              return 'pdf'
+            if (id.includes('qiniu-js'))
+              return 'qiniu'
+            if (id.includes('localforage'))
+              return 'local-storage'
+            if (id.includes('lucide-react'))
+              return 'icons'
+            return 'vendor'
+          }
+        },
       },
     },
   },
